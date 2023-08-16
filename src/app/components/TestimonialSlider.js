1d7d1c4a1dd3b4d31cfe6b4d014e22f1d52feef1
prabirshub <prabirsinghdigital@gmail.com>
2023-08-17 00:16:25 +0530

    update

--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -1,3 +1,8 @@
+'use client'
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 export default function TestimonialSlider() {
   return <div>TestimonialSlider</div>
 }
